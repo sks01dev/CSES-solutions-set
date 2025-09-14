ca1badb3e5c4a7c569156f8d575855b25d6a3da8
SHIVAM KUMAR SWARNKAR <sswarnkar0001@gmail.com>
2025-09-15 00:01:15 +0530

    Update creating strings.cpp

--- creating strings.cpp
+++ creating strings.cpp
@@ -4,15 +4,14 @@ using namespace std;
 int main() {
     string s;
     cin >> s;
-    sort(s.begin(), s.end()); // start with smallest lexicographic
+    sort(s.begin(), s.end()); // start with smallest permutation
     vector<string> res;
-    
-    do {
-        res.push_back(s);
-    } while (next_permutation(s.begin(), s.end())); // generate all unique permutations
-    
+
+    while (true) {
+        res.push_back(s);                 // store current permutation
+        if (!next_permutation(s.begin(), s.end())) break; // generate next one, stop if none
+    }
+
     cout << res.size() << "\n";
     for (auto &x : res) cout << x << "\n";
-    
-    return 0;
 }
